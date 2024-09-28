dcb555f7480871dda8229874068905fd90802814
Emir Burak Önder <100717550+famidok@users.noreply.github.com>
2024-09-28 21:56:48 +0300

    Update troll.c with new features

--- xdp/programs/troll/troll.c
+++ xdp/programs/troll/troll.c
@@ -3,46 +3,51 @@
 #include <linux/ip.h>
 #include <bpf/bpf_helpers.h>
 #include <arpa/inet.h>
+#include <linux/random.h>
 
-#define TARGET_IP 168479233
+#define TARGET_IP 0 // Set the TARGET_IP before running the program
 #define DROP_RATE 10
 
-
 struct vlan_hdr {
-	__be16 h_vlan_TCI;
-	__be16 h_vlan_encapsulated_proto;
+    __be16 h_vlan_TCI;
+    __be16 h_vlan_encapsulated_proto;
 };
 
 SEC("prog")
 int xdp_troll(struct xdp_md *ctx) {
-	void *data = (void *)(long)ctx->data;
-	void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
 
-	struct ethhdr *eth = data;
-        struct vlan_hdr *vhdr = (void *)(eth + 1);
+    struct ethhdr *eth = data;
+    struct vlan_hdr *vhdr;
 
-	if ((void *)(eth + 1) > data_end)
-		return XDP_DROP;
+    // VLAN kontrolü
+    if (eth->h_proto == htons(ETH_P_8021Q)) {
+        vhdr = (struct vlan_hdr *)(eth + 1);
+        if ((void *)(vhdr + 1) > data_end) {
+            return XDP_DROP;
+        }
+    } else {
+        vhdr = (struct vlan_hdr *)(eth + 1); 
+    }
 
-       if (eth->h_proto == htons(ETH_P_8021Q)) {
-       
-	if ((void *)(vhdr + 1) > data_end)
-       		return XDP_DROP;
-	}
-		
-	struct iphdr *iph = (struct iphdr *)(vhdr + 1);
+    struct iphdr *iph = (struct iphdr *)(vhdr + 1);
+    if ((void *)(iph + 1) > data_end) {
+        return XDP_DROP;
+    }
 
-	if((void *)(iph + 1) > data_end)
-		return XDP_DROP;
+    if (iph->daddr != htonl(TARGET_IP)) {
+        return XDP_DROP;
+    }
 
-	if(iph->daddr != TARGET_IP) {
-        	return XDP_DROP;
-    	}
+    u32 rand_num;
+    get_random_bytes(&rand_num, sizeof(rand_num));
+    
+    if (rand_num % DROP_RATE == 0) {
+        return XDP_DROP; 
+    }
 
-
-    	return XDP_PASS;
-
-
-
+    return XDP_PASS; 
 }
+
 char _license[] SEC("license") = "GPL";
